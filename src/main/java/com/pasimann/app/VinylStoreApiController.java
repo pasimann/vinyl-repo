f4ff42fb42e3c53e3e8310eb481f25efe38b5ec0
Pasi Manninen <pasi.manninen@digia.com>
2019-02-07 12:46:41 +0200

    [WIP] added cache to api method.

--- src/main/java/com/pasimann/app/VinylStoreApiController.java
+++ src/main/java/com/pasimann/app/VinylStoreApiController.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.lang.Integer;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,61 +22,61 @@ public class VinylStoreApiController {
     @Autowired
     VinylStoreService service;
 
-    @RequestMapping(value={"/vinyl-store-search-artist"}, method=RequestMethod.GET)
+    @RequestMapping(value = { "/vinyl-store-search-artist" }, method = RequestMethod.GET)
+    @Cacheable(cacheNames = "StoreItem", key = "T(java.util.Objects).hash(#artist, 'searchVinylStoreByArtist')")
     public @ResponseBody List<StoreItem> searchVinylStoreByArtist(
-            @RequestParam(value="artist", required=true) String artist) {
+            @RequestParam(value = "artist", required = true) String artist) {
 
         List<StoreItem> result = service.getArtistVinylItems(artist);
         return result;
     }
 
-    @RequestMapping(value={"/vinyl-store-search-company"}, method=RequestMethod.GET)
+    @RequestMapping(value = { "/vinyl-store-search-company" }, method = RequestMethod.GET)
     public @ResponseBody List<StoreItem> searchVinylStoreByCompany(
-            @RequestParam(value="company", required=true) String company) {
+            @RequestParam(value = "company", required = true) String company) {
 
         List<StoreItem> result = service.findVinylItemsByCompany(company);
         return result;
     }
 
-    @RequestMapping(value={"/vinyl-store-search-title"}, method=RequestMethod.GET)
+    @RequestMapping(value = { "/vinyl-store-search-title" }, method = RequestMethod.GET)
     public @ResponseBody List<StoreItem> searchVinylStoreByTitle(
-            @RequestParam(value="title", required=true) String title) {
+            @RequestParam(value = "title", required = true) String title) {
 
         List<StoreItem> result = service.findVinylItemsByTitle(title);
         return result;
     }
 
-    @RequestMapping(value={"/vinyl-store-search-format"}, method=RequestMethod.GET)
+    @RequestMapping(value = { "/vinyl-store-search-format" }, method = RequestMethod.GET)
     public @ResponseBody List<StoreItem> searchVinylStoreByFormat(
-            @RequestParam(value="format", required=true) String format) {
+            @RequestParam(value = "format", required = true) String format) {
 
         List<StoreItem> result = service.findVinylItemsByFormat(format);
         return result;
     }
 
-    @RequestMapping(value={"/vinyl-store-count-by-artist"}, method=RequestMethod.GET)
-    public @ResponseBody int countVinylItemsByArtist(
-            @RequestParam(value="artist", required=true) String artist) {
+    @RequestMapping(value = { "/vinyl-store-count-by-artist" }, method = RequestMethod.GET)
+    public @ResponseBody int countVinylItemsByArtist(@RequestParam(value = "artist", required = true) String artist) {
 
         int result = service.countVinylItemsByArtist(artist);
         return result;
     }
 
-    @RequestMapping(value={"/vinyl-store-count-total"}, method=RequestMethod.GET)
+    @RequestMapping(value = { "/vinyl-store-count-total" }, method = RequestMethod.GET)
     public @ResponseBody int countVinylDiskTotal() {
 
         int result = service.countVinylDiskTotal();
         return result;
     }
 
-    @RequestMapping(value={"/vinyl-store-summary-by-artist"}, method=RequestMethod.GET)
+    @RequestMapping(value = { "/vinyl-store-summary-by-artist" }, method = RequestMethod.GET)
     public @ResponseBody List<SummaryItem> countVinylSummaryByArtist() {
 
         List<SummaryItem> result = service.countVinylSummaryByArtist();
         return result;
     }
 
-    @RequestMapping(value={"/vinyl-store-summary-by-company"}, method=RequestMethod.GET)
+    @RequestMapping(value = { "/vinyl-store-summary-by-company" }, method = RequestMethod.GET)
     public @ResponseBody List<SummaryItem> countVinylSummaryByCompany() {
 
         List<SummaryItem> result = service.countVinylSummaryByCompany();
